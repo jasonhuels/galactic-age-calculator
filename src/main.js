d570dd40f726bafd690883908561b98cde777238
Jason Huels <jasonhuels@yahoo.com>
2019-09-06 16:20:09 -0700

    add additional message based on number of years left to live

--- src/main.js
+++ src/main.js
@@ -17,26 +17,45 @@ function main() {
       let yearsLeft = "";
       let galactic = new GalacticAge(earthAge, lifeExpectancy);
       const PLANETS = ["mercury", "venus", "mars", "jupiter"];
+      const PAST = "Congratulations on exceeding expectations!";
+      const CLOSE = "Enjoy the time you have left...";
+      const FAR = "You've got plenty of life left to live.";
+      let extra = "";
+
+      if(lifeExpectancy - earthAge < 0) {
+        extra = PAST;
+      } else if(lifeExpectancy - earthAge < 5) {
+        extra = CLOSE;
+      } else {
+        extra = FAR;
+      }
 
       $("#form").trigger("reset");
       if(earthAge >= 1 && lifeExpectancy >=1) {
         $("#error").slideUp();
         if(earthAge >= lifeExpectancy) {
-          yearsLeft = `You've out-lived your life expectancy by ${earthAge - lifeExpectancy} years.`;
+          yearsLeft = `You've out-lived your life expectancy by ${earthAge - lifeExpectancy} years. ${extra}`;
         } else {
-          yearsLeft = `You'll most likely live another ${lifeExpectancy - earthAge} years.`;
+          yearsLeft = `You'll most likely live another ${lifeExpectancy - earthAge} years. ${extra}`;
         }
         $("#earth-age").html(earthAge);
         $("#earth-life").html(lifeExpectancy);
         $("#earth-left").html(yearsLeft);
 
         for(let i=0; i<PLANETS.length; i++) {
+          if(galactic.expectancies[i] - galactic.ages[i] < 0) {
+            extra = PAST;
+          } else if(galactic.expectancies[i] - galactic.ages[i] < 5) {
+            extra = CLOSE;
+          } else {
+            extra = FAR;
+          }
           $(`#${PLANETS[i]}-age`).html(galactic.ages[i]);
           $(`#${PLANETS[i]}-life`).html(galactic.expectancies[i]);
           if(galactic.ages[i] >= galactic.expectancies[i]) {
-            $(`#${PLANETS[i]}-left`).html(`You've out-lived your life expectancy by ${galactic.ages[i] - galactic.expectancies[i]} years.`);
+            $(`#${PLANETS[i]}-left`).html(`You've out-lived your life expectancy by ${galactic.ages[i] - galactic.expectancies[i]} years. ${extra}`);
           } else {
-            $(`#${PLANETS[i]}-left`).html(`You'll most likely live another ${galactic.expectancies[i] -galactic.ages[i]} years.`);
+            $(`#${PLANETS[i]}-left`).html(`You'll most likely live another ${galactic.expectancies[i] -galactic.ages[i]} years. ${extra}`);
           }
         }
         $("#results").slideDown();
