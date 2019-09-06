c01876c5d8e937910e5dc25a0a1453d792fd933b
Jason Huels <jasonhuels@yahoo.com>
2019-09-06 11:05:04 -0700

    start adding ui

--- spec/galactic-age-spec.js
+++ spec/galactic-age-spec.js
@@ -18,13 +18,37 @@ describe("GalacticAge", function() {
   it("will output the user's age on Mercury", function() {
     expect(galactic.mercuryYears(galactic.earthAge)).toEqual(132);
   });
+  it("will output the user's average life expectancy on Mercury", function() {
+    expect(galactic.mercuryYears(galactic.lifeExpectancy)).toEqual(352);
+  });
+  it("will output the user's expected years remaining on Mercury", function() {
+    expect(galactic.mercuryYears(galactic.lifeExpectancy) - galactic.mercuryYears(galactic.earthAge)).toEqual(220);
+  });
   it("will output the user's age on Venus", function() {
     expect(galactic.venusYears(galactic.earthAge)).toEqual(51);
+  });
+  it("will output the user's average life expectancy on Venus", function() {
+    expect(galactic.venusYears(galactic.lifeExpectancy)).toEqual(137);
+  });
+  it("will output the user's expected years remaining on Venus", function() {
+    expect(galactic.venusYears(galactic.lifeExpectancy) - galactic.venusYears(galactic.earthAge)).toEqual(86);
   });
   it("will output the user's age on Mars", function() {
     expect(galactic.marsYears(galactic.earthAge)).toEqual(17);
   });
+  it("will output the user's average life expectancy on Mars", function() {
+    expect(galactic.marsYears(galactic.lifeExpectancy)).toEqual(45);
+  });
+  it("will output the user's expected years remaining on Mars", function() {
+    expect(galactic.marsYears(galactic.lifeExpectancy) - galactic.marsYears(galactic.earthAge)).toEqual(28);
+  });
   it("will output the user's age on Jupiter", function() {
     expect(galactic.jupiterYears(galactic.earthAge)).toEqual(2);
+  });
+  it("will output the user's average life expectancy on Jupiter", function() {
+    expect(galactic.jupiterYears(galactic.lifeExpectancy)).toEqual(7);
+  });
+  it("will output the user's expected years remaining on Jupiter", function() {
+    expect(galactic.jupiterYears(galactic.lifeExpectancy) - galactic.jupiterYears(galactic.earthAge)).toEqual(5);
   });
 });
